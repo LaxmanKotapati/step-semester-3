11a04d4f4b92c79891f71c586f3c7744becd78ad
LaxmanKotapati <kotapatilaxman1@gmail.com>
2026-09-04 21:16:04 +0530

    Add week 4 programs

---
+++ Practice Problems/FeeAccount2.java
@@ -0,0 +1,42 @@
+class FeeAccount {
+    void pay(double amount) {
+        System.out.println("Paid in one go (day-scholar account)");
+    }
+}
+
+class HostelFeeAccount extends FeeAccount {
+    void pay(double amount) {
+        System.out.println("Paid in two installments (hostel account)");
+    }
+}
+
+class Payment {
+    static int hostelCount = 0;
+    static int dayScholarCount = 0;
+
+    static void processPayment(FeeAccount account, double amount) {
+        if (account instanceof HostelFeeAccount) {
+            HostelFeeAccount hostel = (HostelFeeAccount) account;
+            hostel.pay(amount);
+            hostelCount++;
+        } else {
+            account.pay(amount);
+            dayScholarCount++;
+        }
+    }
+
+    public static void main(String[] args) {
+        FeeAccount[] accounts = {
+                new HostelFeeAccount(),
+                new HostelFeeAccount(),
+                new FeeAccount(),
+                new FeeAccount()
+        };
+
+        for (FeeAccount account : accounts)
+            processPayment(account, 60000);
+
+        System.out.println("Hostel accounts processed: " + hostelCount
+                + " | Day-scholar accounts processed: " + dayScholarCount);
+    }
+}
